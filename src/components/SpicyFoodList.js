7250016e200998739ef12b4ecd0ccdb4a2810804
Noor Alrai <ameinnoor91@gmail.com>
2023-10-21 23:12:46 +0300

    Done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,17 +3,52 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [fliterBy ,seFilterBy] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
     console.log(newFood);
   }
+  function handleRemoveFood(id){
+    const newFoodArray = foods.filter(food => food.id !== id)
+    setFoods(newFoodArray);
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+  }
+  function handleUpdate(id){
+
+    const newFoodArray = foods.map(food =>{
+      if(food.id === id ){
+        return { ...food ,
+        heatLevel : food.heatLevel + 1}
+      }else{
+        return food
+      }
+    })
+    setFoods(newFoodArray);
+  }
+  /*function handleFilterBy(){
+    const foodsToDisplay = foods.filter(food => {
+      if(fliterBy === "all"){
+        return true;
+      }
+      else{
+        return (food.cuisine === fliterBy)
+      }
+    })
+ 
+  } */
+
+  const foodList = foods.map((food) => {
+  return ( <li key={food.id} > 
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine} <button onClick = { () => handleUpdate(food.id)}>update</button>
+      <button onClick = { () => handleRemoveFood(food.id)}>delete</button>
+    </li>)
+
+});
+
+
 
   return (
     <div>
